69fb378db96ceedc8ac7906153ec8a64c0c4b25f
ian-hamlin <ian@ihamlin.co.uk>
2020-02-16 13:17:58 +0000

    Unit tests

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,6 @@ fn main() {
     let options: Opts = argh::from_env();
 
     let mut walk = WalkDir::new(options.path);
-
     walk = walk.follow_links(options.follow_links);
 
     if options.max_depth > 0 {
@@ -58,23 +57,82 @@ fn process_file(
     let content = fs::read(entry.path())?;
 
     // Attempt to fix the bom error
-    let content_bomed = if look_at_bom && content.starts_with(b"\xEF\xBB\xBF") {
-        &content[3..]
-    } else if look_at_bom && (content.starts_with(b"\xFF\xFE"))
-        || (content.starts_with(b"\xFE\xFF"))
-    {
-        &content[2..]
-    } else {
-        &content
-    };
-
+    let content_bomed = bom_check(&content, look_at_bom);
     let json: value::Value = serde_json::from_slice(content_bomed)?;
+
     let output = if minify {
         serde_json::to_vec(&json)?
     } else {
         serde_json::to_vec_pretty(&json)?
     };
+
     fs::write(entry.path(), output)?;
 
     Ok(())
+}
+
+fn bom_check(content: &Vec<u8>, look_at_bom: bool) -> &[u8] {
+    if look_at_bom && content.starts_with(b"\xEF\xBB\xBF") {
+        &content[3..]
+    } else if look_at_bom
+        && ((content.starts_with(b"\xFF\xFE")) || (content.starts_with(b"\xFE\xFF")))
+    {
+        &content[2..]
+    } else {
+        &content
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn should_remove_bom_utf8() {
+        let content: Vec<u8> = vec![239, 187, 191, 1];
+        let result = bom_check(&content, true);
+
+        assert_eq!(vec![1], result);
+    }
+
+    #[test]
+    fn should_not_remove_bom_utf8() {
+        let content: Vec<u8> = vec![239, 187, 191, 1];
+        let result = bom_check(&content, false);
+
+        assert_eq!(vec![239, 187, 191, 1], result);
+    }
+
+    #[test]
+    fn should_remove_bom_utf16le() {
+        let content: Vec<u8> = vec![255, 254, 1];
+        let result = bom_check(&content, true);
+
+        assert_eq!(vec![1], result);
+    }
+
+    #[test]
+    fn should_not_remove_bom_utf16le() {
+        let content: Vec<u8> = vec![255, 254, 1];
+        let result = bom_check(&content, false);
+
+        assert_eq!(vec![255, 254, 1], result);
+    }
+
+    #[test]
+    fn should_remove_bom_utf16be() {
+        let content: Vec<u8> = vec![254, 255, 1];
+        let result = bom_check(&content, true);
+
+        assert_eq!(vec![1], result);
+    }
+
+    #[test]
+    fn should_not_remove_bom_utf16lbe() {
+        let content: Vec<u8> = vec![254, 255, 1];
+        let result = bom_check(&content, false);
+
+        assert_eq!(vec![254, 255, 1], result);
+    }
 }
